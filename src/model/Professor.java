cd773b697b3bea237523d1cc9c7a273458f0d59d
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-04-16 09:18:01 -0300

    Adicionando exemplo de Professor

--- src/model/Professor.java
+++ src/model/Professor.java
@@ -1,4 +1,21 @@
 package model;
 
+import java.time.LocalDate;
+
 public class Professor {
+    private String nome;
+    private String cpf;
+    private LocalDate nascimento;
+    private float salario;
+
+    public Professor(String nome, String cpf,
+             LocalDate nascimento, float salario){
+        this.nome = nome;
+        this.cpf = cpf;
+        this.nascimento = nascimento;
+        this.salario = salario;
+    }
+
+    //TODO: Fazer getters e setters
+
 }
