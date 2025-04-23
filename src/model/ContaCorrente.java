8ba35775abc949276cb529e424fec7ba8554c721
Paulo Freitas <paulo.freitas.nt@gmail.com>
2025-04-23 08:53:22 -0300

    Criando classe conta corrente

---
+++ src/model/ContaCorrente.java
@@ -0,0 +1,38 @@
+package model;
+
+public class ContaCorrente {
+    private int numero;
+    private String titular;
+    private float saldo;
+
+    public ContaCorrente(int numero, String titular){
+        this.numero = numero;
+        this.titular = titular;
+        saldo = 0;
+    }
+
+    public boolean depositar(float valor){
+        if(valor > 10000) return false;
+        if(valor < 0) return false;
+        saldo += valor;
+        return true;
+    }
+
+    public boolean sacar(float valor){
+        if(valor < 0) return false;
+        if(valor > saldo) return false;
+        if(saldo == 0) return false;
+        saldo -= valor;
+        return true;
+    }
+
+    public boolean transferir(ContaCorrente destino, float valor){
+        if(valor < 0) return false;
+        if(valor > 10000) return false;
+        if(valor > saldo) return false;
+        saldo -= valor;
+        destino.saldo += valor;
+        return true;
+    }
+
+}
